b476bbaed99894014c27a13ffb0d94ebdaab1662
jaylinkz <johnsonjefferson64@gmail.com>
2022-11-14 17:19:47 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on("connection", (socket) => {
+    socket.broadcast.emit("a new user has connected");
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        console.log('message:' + msg);
+    });
+
+    // socket.on("disconnect", () => {
+    //     console.log("a user has disconnected");
+    // })
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
